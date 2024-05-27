1684e797d4b70c452983f94b02be6259bf268870
bellaciaodowhile <125389712+bellaciaodowhile@users.noreply.github.com>
2024-05-26 20:40:42 -0400

    add nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,44 @@
-const aboutMe = document.querySelector('.experience__aboutme');
+const aboutMe = document.querySelector('.svg__experience');
 
+let nav = document.querySelector('nav');
 let observer = new IntersectionObserver(verifyEntry, {});
 let relativeAboutMe = document.querySelector('.relative__aboutme')
+let sections = [...document.querySelectorAll('section')];
+let itemsMenu = [...document.querySelectorAll('nav li a')];
 observer.observe(aboutMe);
 
 function verifyEntry(entries) {
     let entry = entries[0];
     if (entry.isIntersecting) {
-        console.log('Visible')
-        console.log(relativeAboutMe)
         relativeAboutMe.classList.add('relative__aboutme--active');
-    } else {
-        console.log('Hidden')
     }
 }
+
+function scrollItems() {
+    window.onscroll = (e) => {
+        let exp = document.querySelector('#experiencia');
+        sections.map(section => {
+            const position = section.getBoundingClientRect().top + window.pageYOffset;
+            if ((position - 150) <= window.pageYOffset) {
+                triggerActive(section.id)
+            } 
+            if (window.pageYOffset <= 667) {
+                itemsMenu.map(x => x.classList.remove('a--active') );
+            }
+            function triggerActive(section) {
+                let current = itemsMenu.filter(x => x.innerText.toLowerCase().trim().replace(' ', '-') == `${section}`)[0];
+                if (current != undefined) {
+                    itemsMenu.map(x => x.classList.remove('a--active') );
+                    current.classList.add('a--active');
+                } 
+            }
+        });
+        
+        if (window.pageYOffset > 100) {
+            nav.classList.add('nav--active')
+        } else {
+            nav.classList.remove('nav--active')
+        }
+    }
+}
+scrollItems()
